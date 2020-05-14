1c718eb6f2cdfd4275da8a190a8e3c4af8a12fd6
virmon <virmontumulak@gmail.com>
2020-05-13 20:28:06 -0500

    add json parsing

---
+++ ajax/parse.js
@@ -0,0 +1,17 @@
+$(function() {
+
+fetch("https://jsonplaceholder.typicode.com/users/?userId=1")
+    .then(response => response.json())
+    .then(displayUser);
+
+function displayUser(data) {
+    console.log(data);
+    let user = data.map(x => 
+            "<ul><li>" + x.name + "</li>" + 
+            "<ul><li>" + x.email + "</li>" + 
+            "<li>" + x.address.street + " " + x.address.suite + " " + x.address.city + ", " + x.address.zipcode + "</li></ul></ul>"
+            ).join('');
+    $("#users").html(user);
+}
+
+});
